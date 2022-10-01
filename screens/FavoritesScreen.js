4815d8671b5329ff2f269bb5d1fa1b859c07e0a4
unknown <billajutt435@gmail.com>
2022-10-01 23:54:30 +0500

    Food Menu

---
+++ screens/FavoritesScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View,Text,StyleSheet } from "react-native";
+function FavoritesScreen () {
+    return(
+        <Text></Text>
+    );
+}
+export default FavoritesScreen;
+
+const styles = StyleSheet.create({});
